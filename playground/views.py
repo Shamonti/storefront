5c8593bbb10f42649f53d25165c764b0645d1db0
Shamonti <shamonti.haque98@gmail.com>
2024-07-27 20:41:55 +0600

    Used Django Debug Toolbar

--- playground/views.py
+++ playground/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def say_hello(request):
+  x = 1
+  y = 2
   return render(request, 'hello.html', {'name': 'Shamonti'})
   # return render(request, 'hello.html')
