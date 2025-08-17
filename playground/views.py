fc8a9c880384093f8296a38078068d47c27f2a63
Shamonti <shamonti.haque98@gmail.com>
2025-08-17 10:23:42 +0600

    deleted all old files

--- playground/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def say_hello(request):
-  x = 1
-  y = 2
-  return render(request, 'hello.html', {'name': 'Shamonti'})
-  # return render(request, 'hello.html')
