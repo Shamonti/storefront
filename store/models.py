5bf3a28d43e1dff7e685fbe1aa565434c01568c1
Shamonti <shamonti.haque98@gmail.com>
2025-08-23 13:17:09 +0600

    reverted migration

--- store/models.py
+++ store/models.py
@@ -16,8 +16,9 @@ class Promotion(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now_add=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -85,3 +86,4 @@ class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    zip = models.PositiveIntegerField(null=True)
