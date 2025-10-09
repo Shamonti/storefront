bd7c0772f7ac75e392680f98ed5e817cf5f0f103
Shamonti <shamonti.haque98@gmail.com>
2025-10-09 11:40:37 +0600

    added search filters

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,6 @@
-from django.contrib import admin
+from typing import Any
+from django.contrib import admin, messages
+from django.db.models import Q
 from django.db.models.query import QuerySet
 from django.http.request import HttpRequest
 from . import models
@@ -25,10 +27,34 @@ class CollectionAdmin(admin.ModelAdmin):
         return super().get_queryset(request).annotate(products_count=Count('product'))
 
 
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'Inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<10', 'Low'),
+            ('10<=50', 'Medium'),
+            ('>50', 'High'),
+        ]
+
+    def queryset(self, request, queryset: QuerySet[Any]) -> QuerySet[Any] | None:
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt=10)
+        elif self.value() == '10<=50':
+            return queryset.filter(
+                Q(inventory__gt=10) & Q(inventory__lt=50) | Q(inventory=50)
+            )
+        elif self.value() == '>50':
+            return queryset.filter(inventory__gt=50)
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    actions = ['clear_inventory']
     list_display = ["title", "unit_price", "inventory_status", 'collection_title']
     list_editable = ["unit_price"]
+    list_filter = ['collection', 'last_update', InventoryFilter]
     ordering = ["unit_price"]
     list_per_page = 10
     list_select_related = ['collection']
@@ -43,6 +69,15 @@ class ProductAdmin(admin.ModelAdmin):
         else:
             return "OK"
 
+    @admin.action(description='Clear inventory')
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request,
+            f'{updated_count} products were successfully updated.',
+            messages.ERROR,
+        )
+
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
@@ -50,6 +85,7 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ["membership"]
     ordering = ["first_name", "last_name"]
     list_per_page = 10
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
     def order(self, customer):
         url = (
