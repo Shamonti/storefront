4f9eb27a475ff53e0b661ec84026fbe8332e14b4
Shamonti <shamonti.haque98@gmail.com>
2025-10-27 10:26:28 +0600

    working on serializers

---
+++ store/serializers.py
@@ -0,0 +1,23 @@
+from decimal import Decimal
+from unittest.util import _MAX_LENGTH
+from rest_framework import serializers
+
+from store.models import Product, Collection
+
+
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+
+
+class ProductSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+    price = serializers.DecimalField(
+        max_digits=6, decimal_places=2, source='unit_price'
+    )
+    product_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    collection = CollectionSerializer()
+
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(1.1)
--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('products/', views.product_list),
-    path('products/<int:id>', views.product_details),
+    path('product/<int:id>', views.product_details),
 ]
