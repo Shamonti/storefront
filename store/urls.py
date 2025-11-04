9bdeb5ce004ddf91fccf0db462aae8de937f8e85
Shamonti <shamonti.haque98@gmail.com>
2025-11-04 12:30:53 +0600

    added class based generic view to products and collections

--- store/urls.py
+++ store/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.product_list),
-    path('products/<int:id>', views.product_detail),
+    path('products/', views.ProductList.as_view()),
+    path('products/<int:id>', views.ProductDetails.as_view()),
     path('collections/<int:id>', views.collection_detail, name='collection-detail'),
-    path('collections/', views.collection_list),
+    path('collections/', views.CollectionList.as_view()),
 ]
