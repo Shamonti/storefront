7cfcc702dde85f35b7d517a3c3de6192116f6659
Shamonti <shamonti.haque98@gmail.com>
2025-11-25 13:19:30 +0600

    Added sorting

--- store/views.py
+++ store/views.py
@@ -1,28 +1,24 @@
-from multiprocessing import context
 from typing import Collection, ReadOnly
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.decorators import api_view
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
-
+from rest_framework.filters import SearchFilter, OrderingFilter
 from store.filter import ProductFilter
-from .models import OrderItem, Product, Collection, Review
+from .models import Order, OrderItem, Product, Collection, Review
 from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
 from rest_framework import status
-from rest_framework.mixins import ListModelMixin, CreateModelMixin
-from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from rest_framework.viewsets import ModelViewSet
 from store import serializers
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    search_fields = ['title', 'description']
+    ordering_fields = ['unit_price', 'last_update']
 
     def get_serializer_context(self):
         return {'request': self.request}
