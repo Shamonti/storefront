589d2fde974bb00a1df17d4951b6462d93b2f3e1
Shamonti <shamonti.haque98@gmail.com>
2026-01-12 11:11:25 +0600

    create custom permissions

--- store/models.py
+++ store/models.py
@@ -65,6 +65,9 @@ class Order(models.Model):
     )
     customer = models.ForeignKey("Customer", on_delete=models.PROTECT)
 
+    class Meta:
+        permissions = [('cancel_order', 'Can cancel order')]
+
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = "B"
